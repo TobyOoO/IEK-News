b910e49e0312db614fb8062420c60a4803c21a73
TobyOoO <tobbymailbox@gmai.com>
2016-09-29 18:44:51 +0800

    fix minor bugs

--- IEK-10yr/csv2sql.py
+++ IEK-10yr/csv2sql.py
@@ -4,9 +4,9 @@ import sqlite3
 conn = sqlite3.connect('data/database.db')
 c = conn.cursor()
 
-with open('data/20160929-news.csv', 'rb') as f:
+with open('data/20160929-news.csv', 'r') as f:
 	reader = csv.reader(f)
 	next(reader, None)
 	for i, row in enumerate(reader):
-		c.execute('INSERT INTO News VALUES (?,?,?,?,?,?,?,?,?,?)', row)
+		c.execute('INSERT INTO News VALUES (?,?,?,?,?,?,?,?,?,?,?)', [None]+row)
 		if(i%1000==0):print('inserting %i\r'%i)
