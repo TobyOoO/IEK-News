59b5b9545faaa0e6f3234f6249890065352639bf
TobyOoO <tobbymailbox@gmail.com>
2016-10-15 14:45:13 +0800

    fix bug.

--- IEK-1013/coexit_merge.py
+++ IEK-1013/coexit_merge.py
@@ -13,18 +13,21 @@ for file in os.listdir(folder):
 	if file.endswith(".csv"):
 		with open(folder+file, 'r') as csvfile:
 			reader = csv.reader(csvfile)
-			#header = reader.next()
+			header = reader.next()
 			#print(reader)
-			for i, row in enumerate(reader):
-				print(i)
+			if file.endswith('_0.csv'):
+				target = output_0
+			else:
+				target = output_1
+			for row in reader:
+				target.append(row)
 
-'''
+
 for i in range(2):
-	with open('export_coexist/output_%i.csv'%i, 'w') as f:
+	with open('output_%i.csv'%i, 'w') as f:
 		writer = csv.writer(f)
 		writer.writerow(header)
 		if i == 0:
 			writer.writerows(output_0)
 		else:
 			writer.writerows(output_1)
-'''
