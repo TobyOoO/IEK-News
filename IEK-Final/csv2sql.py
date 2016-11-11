426bb5e3018368bbc6fa5b1baaae327661584e1a
TobyOoO <tobbymailbox@gmail.com>
2016-11-11 23:24:37 +0800

    init IEK final

---
+++ IEK-Final/csv2sql.py
@@ -0,0 +1,15 @@
+import csv
+import sqlite3
+
+conn = sqlite3.connect('data/database.db')
+c = conn.cursor()
+
+with open('data/20160929-news.csv', 'r') as f:
+	reader = csv.reader(f)
+	next(reader, None)
+	for i, row in enumerate(reader):
+		c.execute('INSERT INTO News VALUES (?,?,?,?,?,?,?,?,?,?,?)', [None]+row)
+		if(i%1000==0):
+			print('inserting %i\r'%i)
+			conn.commit()
+conn.close()
