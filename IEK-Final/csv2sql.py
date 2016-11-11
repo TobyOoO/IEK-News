a0c8610a3b3fc5d03d2f0be9ff0d669565b6b347
TobyOoO <tobbymailbox@gmail.com>
2016-11-11 23:26:11 +0800

    revise path.

--- IEK-Final/csv2sql.py
+++ IEK-Final/csv2sql.py
@@ -4,7 +4,7 @@ import sqlite3
 conn = sqlite3.connect('data/database.db')
 c = conn.cursor()
 
-with open('data/20160929-news.csv', 'r') as f:
+with open('data/bigfile/news_raw_data.csv', 'r') as f:
 	reader = csv.reader(f)
 	next(reader, None)
 	for i, row in enumerate(reader):
